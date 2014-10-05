8f6018dbbbf193754f1916979db157359b955bda
Daniel Tralamazza <tralamazza@relayr.de>
2014-10-05 20:31:11 +0200

    readConfig working

--- README.md
+++ README.md
@@ -3,6 +3,16 @@
 Node library for nrf905 transceiver for the RaspberryPI using SPI
 
 
+## Pinout
+
+
+### nrf905 to RPI
+
+* MOSI -> pin 19 / GPIO 10
+* MISO -> pin 21 / GPIO 9
+* CLK -> pin 23 / GPIO 11
+* CSN -> pin 24 / GPIO 8
+
 
 ### Reference
 
--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var nrf905 = require("./lib/nrf905");
 
-
-var radio = nrf905();
+var radio = nrf905({
+	dev: '/dev/spidev0.0',
+	pin_DR: 18, /* gpio 24 */
+	pin_PWR_UP: 16, /* gpio 23 */
+	pin_TRX_CE: 15, /* gpio 22 */
+	pin_TX_EN: 11 /* gpio 17 */
+});
 
 radio.readConfig(function(status, config) {
 	console.log(status, config);
