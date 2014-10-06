87169195115990fe49d2694de52a8bef3fac6abe
Daniel Tralamazza <tralamazza@relayr.de>
2014-10-06 22:55:01 +0200

    use pin 12 for PWR_UP

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var nrf905 = require("./lib");
 var radio = nrf905({
 	dev: '/dev/spidev0.0',
 	pin_DR: 18, /* gpio 24 */
-	pin_PWR_UP: 16, /* gpio 23 */
+	pin_PWR_UP: 12, /* gpio 18 */
 	pin_TRX_CE: 15, /* gpio 22 */
 	pin_TX_EN: 11 /* gpio 17 */
 });
