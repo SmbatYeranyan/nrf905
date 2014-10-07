6ba874b724419f103e7df9ab4fb31d44e1f3d04e
Daniel Tralamazza <tralamazza@relayr.de>
2014-10-07 16:20:24 +0200

    spi and gpio linux modules don't like each other (poop) -> poll DR

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var nrf905 = require("./lib");
 
-var radio = nrf905({
+nrf905({
 	dev: '/dev/spidev0.0',
 	pin_DR: 18, /* gpio 24 */
 	pin_PWR_UP: 12, /* gpio 18 */
 	pin_TRX_CE: 15, /* gpio 22 */
 	pin_TX_EN: 11 /* gpio 17 */
-});
-
-radio.readConfig(function(status, config) {
-	console.log(status, config);
+}, function(err, radio) {
+	radio.readConfig(function(status, config) {
+		console.log('config', config);
+		radio.receive(32, function(data) {
+			console.log('recv', data);
+		});
+	});
 });
