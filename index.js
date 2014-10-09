e34e63328cb02a90ed5e2d9d053d5ad955f89ddd
Daniel Tralamazza <tralamazza@relayr.de>
2014-10-09 12:51:59 +0200

    change TX_EN pin to gpio 23 rewrite .config, now gets and sets depending on args remove EventEmitter code try a new gpio and spi module load a full default config on start

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ nrf905({
 	pin_DR: 18, /* gpio 24 */
 	pin_PWR_UP: 12, /* gpio 18 */
 	pin_TRX_CE: 15, /* gpio 22 */
-	pin_TX_EN: 11 /* gpio 17 */
+	pin_TX_EN: 16 /* gpio 23 */
 }, function(err, radio) {
-	radio.readConfig(function(status, config) {
-		console.log('config', config);
-		radio.receive(32, function(data) {
-			console.log('recv', data);
+	radio.config(0, function(err, config) {
+		console.log('config', err, config);
+		radio.receive(32, function(err, data) {
+			console.log('recv', err, data);
 		});
 	});
 });
